ce5f0d352b193d3b9c6f4ea8fbe136d7ae93510d
A01285958 <a01285958@tec.mx>
2024-05-16 20:55:01 -0600

    c

--- Punto.cpp
+++ Punto.cpp
@@ -35,5 +35,5 @@ double Punto::calculaDistancia(Punto p2){
 }
 
 std::string Punto::str( ){
-    return '(' + std::to_string(x) + ',' std::to_string(y) + ')';
+    return '(' + std::to_string(x) + ',' + std::to_string(y) + ')';
 }
--- Triangulo.hpp
+++ Triangulo.hpp
@@ -3,8 +3,28 @@
 #define Triangulo_hpp
 
 #include <stdio.h>
+#include "Punto.hpp"
 
- 
+ class Triangulo{
+ private: 
+    Punto v1, v2, v3;
+ public: 
+    Triangulo();
+    Trianguilo(Punto _v1, Punto _v2, Punto _v3);
+
+    //Getter
+    Punto getVertice1();
+    Punto getVertice2();
+    Punto getVertice3();
+
+    //Modificadores
+    void setVertice1(Punto _v1);
+    void setVertice2(Punto _v2);
+    void setVertice3(Punto _v3);
+
+    
+
+ };
 
  
 
